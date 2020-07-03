3e53af264609b1f9b62ba4dfd19c3486c5cd729b
LocoField <aiesko@locofield.com>
2020-07-03 23:52:11 +0900

    bug fix addin cancel to call finalize

--- Serial/SerialAddinHelper.cpp
+++ Serial/SerialAddinHelper.cpp
@@ -21,7 +21,6 @@ void SerialAddinHelper::execute()
 	}
 
 	dialog = new QProgressDialog("Addin is running ...", "Cancel", 0, 100);
-	dialog->setAutoReset(false);
 	dialog->setMaximum(addin->maximum());
 	QObject::connect(dialog, &QProgressDialog::canceled, this, &SerialAddinHelper::cancel);
 
@@ -31,6 +30,11 @@ void SerialAddinHelper::execute()
 	timer->start(0);
 
 	dialog->exec();
+
+	while (addin->finished() == false)
+	{
+		Sleep(1000);
+	}
 
 	timer->stop();
 
@@ -55,12 +59,6 @@ void SerialAddinHelper::perform()
 
 	addin->callback();
 
-	if (addin->finished())
-	{
-		dialog->reset();
-		return;
-	}
-
 	int value = addin->value();
 	dialog->setValue(value);
 
@@ -74,4 +72,22 @@ void SerialAddinHelper::perform()
 void SerialAddinHelper::cancel()
 {
 	addin->cancel();
+	addin->callback();
+
+	while (addin->sendQueue.size() > 0)
+	{
+		auto& data = addin->sendQueue.front();
+		serialPorts[data.index]->write({ data.buffer.data(), (int) data.buffer.size() });
+	}
+
+	for (size_t i = 0; i < serialPorts.size(); )
+	{
+		QByteArray buffer = serialPorts[i]->read(1);
+		if (buffer.isEmpty())
+		{
+			i++;
+		}
+
+		addin->recvQueue.push_back({ i,{ buffer.begin(), buffer.end() } });
+	}
 }
